f1c1f313d1b0f9e3ec8b0ed3554c300d98ae5258
Asif Muhammed <asifasifpsps@gmail.com>
2023-11-15 11:53:21 +0530

    recursion formatted

--- Javascript/binarySearch.js
+++ Javascript/binarySearch.js
@@ -1,19 +1,19 @@
 
 
-function binarySearch(arr,n){
+function binarySearch(arr, n) {
 
     let leftIndex = 0
-    let rightIndex = arr.length -1
+    let rightIndex = arr.length - 1
 
-    while(leftIndex <= rightIndex){
+    while (leftIndex <= rightIndex) {
         let middleIndex = Math.floor((leftIndex = rightIndex) / 2)
     }
 
 
 
-      let newArr =  arr.sort()
-    for(i=0;i<newArr.length;i++){
-        if(newArr[i]===n){
+    let newArr = arr.sort()
+    for (i = 0; i < newArr.length; i++) {
+        if (newArr[i] === n) {
             return i
         }
     }
@@ -22,4 +22,4 @@ function binarySearch(arr,n){
 
 }
 
-console.log(binarySearch([3,2,5,4,8,6],5))
+console.log(binarySearch([3, 2, 5, 4, 8, 6], 5))
