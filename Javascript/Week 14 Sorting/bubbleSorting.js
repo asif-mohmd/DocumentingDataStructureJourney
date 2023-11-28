712c1ac9462a18fbf4957aa73e29af94136b15d3
Asif Muhammed <asifasifpsps@gmail.com>
2023-11-28 14:34:00 +0530

    insertion sort

--- Javascript/Week 14 Sorting/bubbleSorting.js
+++ Javascript/Week 14 Sorting/bubbleSorting.js
@@ -1,23 +1,23 @@
 
 
-function bubbleSort(arr){
+function bubbleSort(arr) {
     let swapped
 
-    do{
-         swapped = false
-        for(let i=0;i<arr.length-1 ;i++){
-           if(arr[i]>arr[i+i]){
-            let temp = arr[i]
-            arr[i] = arr[i+1]
-            arr[i+1] = temp
-            swapped = true
-           }
+    do {
+        swapped = false
+        for (let i = 0; i < arr.length - 1; i++) {
+            if (arr[i] > arr[i + 1]) {
+                let temp = arr[i]
+                arr[i] = arr[i + 1]
+                arr[i + 1] = temp
+                swapped = true
+            }
 
         }
-    }while(swapped)
+    } while (swapped)
 }
 
-let arr = [2,3,-1,1,5,-4,4]
+let arr = [2, 3, -1, 1, 5, -4, 4]
 
 bubbleSort(arr)
 console.log(arr)
--- Javascript/Week 14 Sorting/insertionSort.js
+++ Javascript/Week 14 Sorting/insertionSort.js
@@ -1,3 +1,22 @@
-function insertionSort(){
-    
+function insertionSort(arr){
+
+    for(let i=0;i<arr.length;i++){
+        let  numToInsert = arr[i]
+        let j = i-1
+
+        while(j>=0 && arr[j] > numToInsert){
+            arr[j+1] = arr[j]
+            j = j-1
+        }
+
+        arr[j+1] = numToInsert
+    }
+
 }
+
+
+let arr = [-6,3,-2,1,9,5,7,-4]
+
+insertionSort(arr)
+
+console.log(arr)
