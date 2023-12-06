ddaa3548791931d11869bfbaec34eb0493a8ffbe
Asif Muhammed <asifasifpsps@gmail.com>
2023-12-06 20:01:37 +0530

    binary treee node insertion

--- Javascript/Tree & Graph/binaryTree.js
+++ Javascript/Tree & Graph/binaryTree.js
@@ -15,6 +15,36 @@ class BinarySearchTree{
     isEmpty(){
         return this.root === null
     }
+
+    insert(value){
+        const newNode = new Node(value)
+        if(this.isEmpty()){
+            this.root = newNode
+        }else{
+            this.insertNode(this.root,newNode) 
+        }
+    }
+
+
+    insertNode(root,newNode){
+        if(newNode.value<root.value){
+            if(root.left === null){
+                root.left = newNode
+            }else{
+                this.insertNode(root.left,newNode)
+            }
+
+        }else{
+            if(root.right==null){
+                root.right = newNode
+            }else{
+                this.insertNode(root.right,newNode)
+            }
+        }
+    }
+
+
+
 }
 
 const bst = new BinarySearchTree()
