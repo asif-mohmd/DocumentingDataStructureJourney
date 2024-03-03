64c46f0f4abd0422fce60888598fbea26648dd8c
asif-mohmd <asifasifpsps@gmail.com>
2024-03-03 20:13:15 +0530

    Deleted a file fibanocci

--- Javascript/test2.js
+++
@@ -1,9 +0,0 @@
-function fibonacci(n){
-    const fib = [0,1]
-    for(let i=2;i<n;i++){
-        fib[i] = fib[i-1] + fib[i-2]
-    }
-    return fib
-}
-
-console.log(fibonacci(5))
--- workout.js
+++ workout.js
@@ -1,1 +1,23 @@
-console.log("test")
+
+
+function palindromCheck(str){
+
+    let left = 0
+    let right = str.length-1
+
+    while(left<right){
+        if(str[left]!= str[right]){
+            return false
+        }
+        left++
+        right--
+    }
+    return true
+}
+
+
+
+
+let str = "malayalfdgfdgdam"
+
+console.log(palindromCheck(str))
